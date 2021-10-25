b0c397d38c9300c38e3d10dfb4a7e55c08137a18
slava2009 <43423776+slava2009@users.noreply.github.com>
2021-10-25 19:01:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <a
           className="App-link"
           href="https://calendar.google.com/calendar/embed?src=pj4va06gncb1vgv76864hbsh08%40group.calendar.google.com&ctz=Europe%2FMoscow"
-          padding-bottom="80%"
-          padding-top= "50px"
+          padding-bottom="56%"
+          padding-top= "40px"
           height= "0"
           overflow= "hidden"
           target="_blank"
