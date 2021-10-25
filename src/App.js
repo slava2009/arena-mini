67139c6c5d7e033b7ca8e53990847ca5991da95f
slava2009 <43423776+slava2009@users.noreply.github.com>
2021-10-25 19:15:56 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Расписание Малой арены </code>
+        </p>
         <a
           className="App-link"
           href="https://calendar.google.com/calendar/embed?src=pj4va06gncb1vgv76864hbsh08%40group.calendar.google.com&ctz=Europe%2FMoscow"
-          padding-bottom="56%"
-          padding-top= "40px"
+          padding-bottom="80%"
+          padding-top= "50px"
           height= "0"
           overflow= "hidden"
           target="_blank"
